7277ae2e1a5a44da960fa569eafb03d8552acd4e
Adrian-Fernandez-Rosa <adrianfr2011@gmail.com>
2025-10-04 16:25:53 +0200

    wii

--- Hello World/lib/presentation/screens/counter/counter_functions_screen.dart
+++ Hello World/lib/presentation/screens/counter/counter_functions_screen.dart
@@ -16,74 +16,79 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
     //  final palabra = clickCounter % 2 == 0 ? 'Clicks' : 'Click';
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter Functions'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh_rounded),
-            onPressed: () {
-              setState(() {
-                clickCounter = 0;
-              });
-            },
-          )
-        ],
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('$clickCounter',
-              style:
-                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-          Text('Click${clickCounter == 1 ? '' : 's'}',
-              style: const TextStyle(fontSize: 25))
-        ],
-      )),
-
-      
-      floatingActionButton: Column(
+        appBar: AppBar(
+          title: const Text('Counter Functions'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            )
+          ],
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$clickCounter',
+                style: const TextStyle(
+                    fontSize: 160, fontWeight: FontWeight.w100)),
+            Text('Click${clickCounter == 1 ? '' : 's'}',
+                style: const TextStyle(fontSize: 25))
+          ],
+        )),
+        floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              shape: const StadiumBorder(),
+            CustomButton(
+              icon: Icons.refresh_rounded,
               onPressed: () {
-                clickCounter++;
-
-                // palabra = clickCounter % 2 == 0 ? 'Clicks' : "click";
-
+                clickCounter = 0;
                 setState(() {});
               },
-              child: const Icon(Icons.refresh_outlined),
             ),
-
-          const SizedBox(height: 10,),
-                 FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                clickCounter++;
-
-                // palabra = clickCounter % 2 == 0 ? 'Clicks' : "click";
-
-                setState(() {});
-              },
-              child: const Icon(Icons.plus_one),
+            const SizedBox(
+              height: 10,
             ),
-
-             const SizedBox(height: 10,),
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-
+            CustomButton(
+              icon: Icons.exposure_minus_1_outlined,
               onPressed: () {
+                if (clickCounter == 0) return;
                 clickCounter--;
-
-                // palabra = clickCounter % 2 == 0 ? 'Clicks' : "click";
-
                 setState(() {});
               },
-              child: const Icon(Icons.exposure_minus_1_outlined),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                clickCounter++;
+                setState(() {});
+              },
             ),
           ],
         ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomButton({super.key, required this.icon, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+    //  shape: const StadiumBorder(),
+      enableFeedback: true,
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
   }
 }
