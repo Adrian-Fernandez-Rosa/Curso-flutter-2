df4b234c69caf205bdf88df02fb38aea77f884ea
Adrian-Fernandez-Rosa <adrianfr2011@gmail.com>
2025-10-05 23:47:44 +0200

    form field

--- yes_or_no/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_or_no/lib/presentation/widgets/shared/message_field_box.dart
@@ -5,6 +5,47 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Holuus");
+    // final colores = Theme.of(context).colorScheme;
+
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final OutlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final InputDecorationd = InputDecoration(
+      hintText: 'Envia tu mensaje como pregunta ??',
+      enabledBorder: OutlineInputBorder,
+      focusedBorder: OutlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Valor de la caja de texto');
+          textController.clear();
+        },
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: InputDecorationd,
+      onFieldSubmitted: (value) {
+        print("submit value $value");
+        textController.clear(); // Limpia lo que escribio
+        focusNode
+            .requestFocus(); // no se notan cambios, se supone que conserva el foco
+      },
+      onChanged: (value) {
+        print('changed: $value');
+      },
+    );
   }
 }
